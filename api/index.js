ad02aa4d37a07fb4e63a4dbae668028f040a0a5d
joel mathias <joelmathias.forever9@gmail.com>
2024-09-07 18:56:34 -0300

    1.0.1

---
+++ api/index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+const app = express();
+
+app.use(json());
+
+app.get('/', (req, res) => {
+  res.send('Hello from Express.js!');
+});
+
+app.get('/api/hello', (req, res) => {
+  res.json({ message: 'Hello World from Vercel Serverless!' });
+});
+
+export default app;
